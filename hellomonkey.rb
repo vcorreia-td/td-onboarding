a029b15029b9b9842d20634831753c4195fb382b
Vasco Correia <vasco.correia@talkdesk.com>
2018-02-27 18:04:32 +0000

    added endpoint

--- hellomonkey.rb
+++ hellomonkey.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
+get '/' do
+  'Index Page'
+ends
+
 get '/hello-monkey' do
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say 'Hello Monkey'
