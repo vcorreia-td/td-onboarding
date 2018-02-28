81d2ce86b125362c7ac500005e299f9b7c78b14c
Vasco Correia <vasco.correia@talkdesk.com>
2018-02-28 15:49:34 +0000

    fix

--- fullfeatured.rb
+++ fullfeatured.rb
@@ -56,7 +56,7 @@ get '/talking-to-person' do
                   statusCallbackMethod: 'POST',
                   statusCallbackEvent: 'completed')
     end
-    r.redirect('./hangup-notification')
+    r.redirect('/hangup-notification')
     # r.say('Goodbye person')
   end.to_s
 end
