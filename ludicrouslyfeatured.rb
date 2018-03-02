4b8fe673a75664af3438aba04a9288325586c8cb
Vasco Correia <vasco.correia@talkdesk.com>
2018-03-02 10:30:19 +0000

    fix

--- ludicrouslyfeatured.rb
+++ ludicrouslyfeatured.rb
@@ -3,18 +3,26 @@ require 'sinatra'
 require 'twilio-ruby'
 require 'securerandom'
 
+if File.file? '.env'
+  open('.env', 'r').readlines.each {|l| kv = l.split('='); ENV[kv[0]] = kv[1];}
+end
+
 db = {}
 client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+puts client
+
+# domain = 'https://secret-shelf-83431.herokuapp.com'
+domain = 'http://routing.tst.talkdeskdev.com:9221'
 
 get '/interaction' do
   interaction_id = SecureRandom.uuid
   room_id = SecureRandom.uuid
   call_sid = params['CallSid']
   db[interaction_id] = [call_sid]
-  other_call = client.calls.create(url: "https://secret-shelf-83431.herokuapp.com/interaction/conference/#{interaction_id}",
-                                 to: '+351937753869',
-                                 #from: params['From'])
-                                 from: '+19783062153')
+  other_call = client.calls.create(url: "#{domain}/interaction/conference/#{interaction_id}",
+                                   to: '+351937753869',
+                                   #from: params['From'])
+                                   from: '+19783062153')
   db[interaction_id] = [call_sid]
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say('You are going to talk to a person')
@@ -23,7 +31,7 @@ get '/interaction' do
                       start_conference_on_enter: true,
                       end_conference_on_exit: false,
                       # hack :(
-                      statusCallback: "https://secret-shelf-83431.herokuapp.com/interaction/hangup/#{interaction_id}",
+                      statusCallback: "#{domain}/interaction/hangup/#{interaction_id}",
                       statusCallbackMethod: 'POST',
                       status_callback_event: 'leave')
     end
@@ -41,7 +49,7 @@ post '/interaction/conference/:interaction_id' do
                       start_conference_on_enter: true,
                       end_conference_on_exit: false,
                       # hack :(
-                      statusCallback: "https://secret-shelf-83431.herokuapp.com/interaction/hangup/#{interaction_id}",
+                      statusCallback: "#{domain}/interaction/hangup/#{interaction_id}",
                       statusCallbackMethod: 'POST',
                       status_callback_event: 'leave')
     end
@@ -54,9 +62,10 @@ post '/interaction/hangup/:interaction_id' do
   db[interaction_id].delete(call_sid)
   db[interaction_id].each do |cs|
     call = client.api.calls(cs)
-    call.update(url: 'https://secret-shelf-83431.herokuapp.com/interaction/goodbye',
+    call.update(url: "#{domain}/interaction/goodbye",
                 method: 'GET')
   end
+  # clean DB
 end
 
 get '/interaction/goodbye' do
