204fdfd939ac21333647a168bba80d0f033d209c
Vasco Correia <vasco.correia@talkdesk.com>
2018-03-05 16:35:47 +0000

    using mongo for db

--- ludicrouslyfeatured.rb
+++ ludicrouslyfeatured.rb
@@ -2,30 +2,55 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 require 'securerandom'
+require 'mongo'
 
 if File.file? '.env'
   open('.env', 'r').readlines.each {|l| kv = l.split('='); ENV[kv[0]] = kv[1].strip;}
 end
 
-db = {}
-client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
-puts ENV['ACCOUNT_SID']
-puts ENV['AUTH_TOKEN']
-puts client
 
+# DB stuff
+
+mongo_client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'myCentral')
+$db = mongo_client[:interactions]
+
+def create_interaction(interaction_id, *call_sids)
+  interaction = {interaction_id: interaction_id, call_sids: call_sids}
+  $db.insert_one(interaction)
+end
+
+def destroy_interaction(interaction_id)
+  $db.delete_one( {interaction_id: interaction_id} )
+end
+
+def add_call_to_interaction(interaction_id, call_sid)
+  interaction = $db.find( {interaction_id: interaction_id} ).first
+  call_sids = interaction['call_sids']
+  call_sids.push(call_sid)
+  $db.update_one( {'interaction_id' => interaction_id}, 
+                 {'$set' => {call_sids: call_sids}})
+end
+
+def get_calls_in_interaction(interaction_id)
+  interaction = $db.find( {interaction_id: interaction_id} )
+  interaction = interaction.first
+  interaction ? interaction['call_sids'] : []
+end
+
+
+# routes
+
+client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
 # domain = 'https://secret-shelf-83431.herokuapp.com'
 domain = 'http://routing.tst.talkdeskdev.com:9221'
 
 get '/interaction' do
   interaction_id = SecureRandom.uuid
-  room_id = SecureRandom.uuid
   call_sid = params['CallSid']
-  db[interaction_id] = [call_sid]
+  create_interaction(interaction_id, call_sid)
   other_call = client.calls.create(url: "#{domain}/interaction/conference/#{interaction_id}",
                                    to: '+351937753869',
-                                   #from: params['From'])
                                    from: '+19783062153')
-  db[interaction_id] = [call_sid]
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say('You are going to talk to a person')
     r.dial do |dial|
@@ -43,7 +68,7 @@ end
 post '/interaction/conference/:interaction_id' do
   interaction_id = params['interaction_id']
   call_sid = params['CallSid']
-  db[interaction_id].push(call_sid)
+  add_call_to_interaction(interaction_id, call_sid)
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say('Prepare to talk to a person')
     r.dial do |dial|
@@ -61,13 +86,14 @@ end
 post '/interaction/hangup/:interaction_id' do
   interaction_id = params['interaction_id']
   call_sid = params['CallSid']
-  db[interaction_id].delete(call_sid)
-  db[interaction_id].each do |cs|
+  call_sids = get_calls_in_interaction(interaction_id)
+  call_sids.delete(call_sid)
+  call_sids.each do |cs|
     call = client.api.calls(cs)
     call.update(url: "#{domain}/interaction/goodbye",
                 method: 'GET')
-  end
-  # clean DB
+  end  
+  destroy_interaction(interaction_id)
 end
 
 get '/interaction/goodbye' do
